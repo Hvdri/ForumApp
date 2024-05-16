6641260142fd141eb37d91b65dd0ee14b9d9ebf1
Robert Schmidt <robert.schmidt@nevexo.ro>
2024-05-16 13:18:06 +0300

    Add ASP.NET project with db integration

---
+++ Discussion-Forum/Database/ForumDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Discussion_Forum.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discussion_Forum.Database
+{
+    public class ForumDbContext : DbContext
+    {
+        public ForumDbContext()
+        {
+        }
+        public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
