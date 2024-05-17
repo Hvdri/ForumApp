729ba17350cb41c4509244ff464dc03c22f06a00
Robert Schmidt <robert.schmidt@nevexo.ro>
2024-05-17 17:27:43 +0300

    Change to .net core + react project

--- Discussion-Forum/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Discussion_Forum.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
